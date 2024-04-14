f8efe80a6979a340061e64f30154131c50e9e7c9
RoelYoon <roelyoon2@gmail.com>
2024-04-15 07:16:58 +0900

    Uploaad

--- public/index.js
+++ public/index.js
@@ -1,2 +1,11 @@
 let leftArrow = document.getElementById("leftArrow");
 let rightArrow = document.getElementById("rightArrow");
+function goInd1(){
+    document.location.href = "http://35.203.145.230:8089/index.html";
+}
+function goInd2(){
+    document.location.href = "http://35.203.145.230:8089/index2.html";
+}
+function goInd3(){
+    document.location.href = "http://35.203.145.230:8089/index3.html";
+}
