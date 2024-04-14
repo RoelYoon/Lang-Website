d6b02082487ab8592fef9f5eb96363287bf5b271
RoelYoon <roelyoon2@gmail.com>
2024-04-15 07:26:02 +0900

    Uploaad

--- public/index.js
+++ public/index.js
@@ -1,11 +1,25 @@
 let leftArrow = document.getElementById("leftArrow");
 let rightArrow = document.getElementById("rightArrow");
-function goInd1(){
-    document.location.href = "http://35.203.145.230:8089/index.html";
+var ind = 0;
+function leftArrClick(){
+    if(ind>0){
+        ind--;
+        if(ind==0){
+            document.location.href = "http://35.203.145.230:8099/index.html";
+        }else{
+            document.location.href = "http://35.203.145.230:8099/index2.html";
+        }
+    }
 }
-function goInd2(){
-    document.location.href = "http://35.203.145.230:8089/index2.html";
+function rightArrClick(){
+    if(ind<2){
+        ind++;
+        if(ind==1){
+            document.location.href = "http://35.203.145.230:8099/index3.html";
+        }else{
+            document.location.href = "http://35.203.145.230:8099/index2.html";
+        }
+    }
 }
-function goInd3(){
-    document.location.href = "http://35.203.145.230:8089/index3.html";
-}
+leftArrow.onclick = leftArrClick;
+rightArrow.onclick = rightArrClick;
