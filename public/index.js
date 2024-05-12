440935a3c90e0516e1b71f381130d7e4b0dd1469
RoelYoon <roelyoon2@gmail.com>
2024-05-12 17:41:46 +0900

    M

---
+++ public/index.js
@@ -0,0 +1,4 @@
+var image = document.getElementById("mail");
+image.onclick = function(e) {
+  image.src="https://docs.google.com/drawings/d/e/2PACX-1vQMxg6xzSnqeT82D4wYdB2zePLXPmcRcdsZGa5wWTmLJCo5lmWkb4cPyJz1n1hhoA8pYbUCFbA2-Q96/pub?w=960&h=720";
+}
