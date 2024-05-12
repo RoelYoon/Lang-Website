2243674b2eeb881cbe7c5edccb7b38f5475d9ac0
RoelYoon <roelyoon2@gmail.com>
2024-05-12 17:45:03 +0900

    M

--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ image.onclick = function(e) {
   image.src="https://docs.google.com/drawings/d/e/2PACX-1vQMxg6xzSnqeT82D4wYdB2zePLXPmcRcdsZGa5wWTmLJCo5lmWkb4cPyJz1n1hhoA8pYbUCFbA2-Q96/pub?w=960&h=720";
         open=true;
 }else{
-    var pos = image.position();
+    var pos = $(this).position();
     //The following are the x/y coordinates of the mouse click relative to image.
     var x = e.pageX - pos.left;
     var y = e.pageY - pos.top;
